236757c501a532c2573c748590fda252c4b500ca
Bach Nguyen <73509508+Bach2324@users.noreply.github.com>
2021-01-15 17:50:55 -0700

    Adding comments

--- ParityParty.py
+++ ParityParty.py
@@ -1,14 +1,14 @@
 def ParityParty(d):
-    a_list = []
+    a_list = []                     # Declaring our list to store the values
 
-    if (d % 2 == 0):
-        a_list.append(0)
-        a_list.append(int(d/2))
-    else:
-        a_list.append(1)
-        a_list.append(int((d-1)/2))
+    if (d % 2 == 0):                # Evaluate if the argument is an even number
+        a_list.append(0)            # Append 0 to the first index of the list
+        a_list.append(int(d/2))     # Append the value divided by 2 (convert to int type) to the second index
+    else:                           # Evaluate if argument is odd
+        a_list.append(1)            # Append 1 to the first index of the list
+        a_list.append(int((d-1)/2)) # Append value minus 1 and divide by 2 to the second index
     
-    return a_list
+    return a_list                   # Return the list
 
-print(ParityParty(10))
-print(ParityParty(33))
+print(ParityParty(10))              # Test case 1
+print(ParityParty(33))              # Test case 2
